0599f8fc2369872d86089d26021c2918df551d96
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-12-16 17:48:25 -0700

    First commit

--- QrCodeGen/Form1.Designer.cs
+++ QrCodeGen/Form1.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            qrCodeBox1 = new PictureBox();
+            userInputBox = new TextBox();
+            buttonGen = new Button();
+            buttonSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)qrCodeBox1).BeginInit();
             SuspendLayout();
+            // 
+            // qrCodeBox1
+            // 
+            qrCodeBox1.Location = new Point(579, 50);
+            qrCodeBox1.Name = "qrCodeBox1";
+            qrCodeBox1.Size = new Size(180, 187);
+            qrCodeBox1.TabIndex = 0;
+            qrCodeBox1.TabStop = false;
+            // 
+            // userInputBox
+            // 
+            userInputBox.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            userInputBox.Location = new Point(24, 66);
+            userInputBox.Name = "userInputBox";
+            userInputBox.Size = new Size(526, 63);
+            userInputBox.TabIndex = 1;
+            userInputBox.Text = "www.google.com";
+            // 
+            // buttonGen
+            // 
+            buttonGen.Location = new Point(24, 164);
+            buttonGen.Name = "buttonGen";
+            buttonGen.Size = new Size(166, 62);
+            buttonGen.TabIndex = 2;
+            buttonGen.Text = "Generate QR Code";
+            buttonGen.UseVisualStyleBackColor = true;
+            buttonGen.Click += buttonGen_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(216, 164);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(166, 62);
+            buttonSave.TabIndex = 3;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(864, 495);
+            ClientSize = new Size(800, 280);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonGen);
+            Controls.Add(userInputBox);
+            Controls.Add(qrCodeBox1);
             Name = "Form1";
             Text = "Qr Code Generator";
+            ((System.ComponentModel.ISupportInitialize)qrCodeBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox qrCodeBox1;
+        private TextBox userInputBox;
+        private Button buttonGen;
+        private Button buttonSave;
     }
 }
--- QrCodeGen/Form1.cs
+++ QrCodeGen/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Net.Http;
+using System.Windows.Forms;
+
 namespace QrCodeGen
 {
     public partial class Form1 : Form
@@ -6,5 +10,50 @@ namespace QrCodeGen
         {
             InitializeComponent();
         }
+               
+        private async void buttonGen_Click(object sender, EventArgs e)
+        {
+            // Get the text value from userInputBox
+            string userInput = userInputBox.Text;
+
+            // Construct the API URL with the user input
+            string qrCodeUrl = $"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={userInput}";
+
+            // Use HttpClient to make the API request
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Send a GET request to the API
+                    HttpResponseMessage response = await client.GetAsync(qrCodeUrl);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Get the response content as a byte array
+                        byte[] imageData = await response.Content.ReadAsByteArrayAsync();
+
+                        // Create a MemoryStream from the byte array
+                        using (System.IO.MemoryStream ms = new System.IO.MemoryStream(imageData))
+                        {
+                            // Set the image in qrCodeBox1
+                            qrCodeBox1.Image = System.Drawing.Image.FromStream(ms);
+                        }
+                    }
+                    else
+                    {
+                        // Handle the case when the API request is not successful
+                        MessageBox.Show($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+
+
     }
 }
